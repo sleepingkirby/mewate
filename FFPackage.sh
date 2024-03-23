4227bf0176f2a8dd62d5cc711dc0fe7ff2fcb537
sleepingkirby <sleepingkirby@gmail.com>
2024-03-23 21:52:42 +0800

    fixed FFpackage.sh and adding release

--- FFPackage.sh
+++ FFPackage.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-zip -r -FS -Z deflate ./releases/butWhyMod.xpi backgrouund.js ./content_scripts/ icons/ manifest.json options/ popup/
+zip -r -FS -Z deflate ./releases/mewate.xpi background.js ./content_scripts/ icons/ manifest.json popup/
